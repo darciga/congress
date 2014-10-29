04031de16f54e1aa8bdf424ee3a395dd2178f238
Dante Omar <darciga@tingsystems.com>
2014-10-29 13:22:33 -0600

    Primera api del modelo applicant

--- congreso/urls.py
+++ congreso/urls.py
@@ -1,10 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from rest_framework import routers
+from congress.views import ApplicantViewSet
+
+router = routers.DefaultRouter()
+router.register(r'applicants', ApplicantViewSet)
+router.register(r'applicants', ApplicantViewSet)
+
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'congress.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    #rutas de las api
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth', include('rest_framework.urls',namespace='rest_framework')),
 )
--- congress/views.py
+++ congress/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from .models import Conference, Applicants, Workshops, Rapporteur
-
+from .serializer import AplicantsSerializer
+from rest_framework import viewsets
 
 def index(request):
     workshops = Workshops.objects.all()
@@ -9,3 +10,8 @@ def index(request):
 
     template = "index.html"
     return render_to_response(template, context_instance=RequestContext(request, locals()))
+
+#Vista para el serializer
+class ApplicantViewSet(viewsets.ModelViewSet):
+    queryset = Applicants.objects.all()
+    serializer_class = AplicantsSerializer
